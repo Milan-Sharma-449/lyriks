584def036fb1c994117cc26963af617314613059
Milan-Sharma-449 <milansharma449@gmail.com>
2022-12-16 12:29:19 +0530

    created hesder of details component

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,20 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
-);
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DetailsHeader = ({ artistId, artistData, songData }) => (
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
+
+    <div className="absolute inset-0 flex items-center">
+      <img
+        alt="profile"
+        src={
+          artistId ? artistData?.artists[artistId].attributes?.artwork?.url
+            .replace('{w}', '500')
+            .replace('{h}', '500')
+            : songData?.images?.coverart
+}
+        className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
+      />
 
 export default DetailsHeader;
