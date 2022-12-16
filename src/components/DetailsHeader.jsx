7293fa446c81c8e657da8328a6e17f038d74ef2d
Milan-Sharma-449 <milansharma449@gmail.com>
2022-12-16 12:29:51 +0530

    fully developed detailsheader

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -17,4 +17,26 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
         className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
       />
 
+      <div className="ml-5">
+        <p className="font-bold sm:text-3xl text-xl text-white">
+          {artistId ? artistData?.artists[artistId].attributes?.name : songData?.title}
+        </p>
+        {!artistId && (
+          <Link to={`/artists/${songData?.artists[0]?.adamid}`}>
+            <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
+          </Link>
+        )}
+
+        <p className="text-base text-gray-400 mt-2">
+          {artistId
+            ? artistData?.artists[artistId].attributes?.genreNames[0]
+            : songData?.genres?.primary}
+        </p>
+      </div>
+    </div>
+
+    <div className="w-full sm:h-44 h-24" />
+  </div>
+);
+
 export default DetailsHeader;
